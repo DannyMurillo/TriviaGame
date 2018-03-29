0ca649939df5a5ce9fed5222e141f84198a4ecac
Daniel Murillo <danielmurillo@Daniels-MacBook-Pro.local>
2018-03-28 18:00:53 -0700

    changed the layout to a different set up

--- README.md
+++ README.md
@@ -1,3 +1,9 @@
 # TriviaGame
 Hw 5
 
+For the most part i got the assignmet. I kept redoing the code trying to follow different ways of assembling the site opting to eventually do the more difficult quiz. I kept running into the problem 
+
+"Uncaught TypeError: Cannot set property 'innerHTML' of null
+    at showQuestions" i looked it up and couldnt understand what it said. 
+
+The timer also wasnt function properly at first but im working on fixing it at the moment. 
--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -20,147 +20,122 @@ function CheckTime () {
 setTimeout("CheckTime()",1000);
 
 // all of the questions that will be used in the quiz
-    var myQuestions = [
-      {
-        question: "Who is the last Premier League team to go undefeated?",
-        answers: {
-          a: 'Manchester United',
-          b: 'Manchester City',
-          c: 'Arsenal'
-        },
-        correctAnswer: 'c'
-      },
-      {
-        question: "Who is the most recent player to average a triple double in the regular NBA season?",
-        answers: {
-          a: 'Russell Westbrook',
-          b: 'Lebron James',
-          c: 'James Harden'
-        },
-        correctAnswer: 'a'
-      },
-      {
-        question: "Which WWE wrestler is not a big movie actor today?",
-        answers: {
-          a: 'Batista',
-          b: 'Rey Mysterio',
-          c: 'Dwayne The Rock Johnson',
-        },
-        correctAnswer: 'b'
-      },
-      {
-        question: "Which NALCS team is the first to knockout TSM out of the playoffs in a round that is not the final?",
-        answers: {
-          a: 'Cloud 9',
-          b: 'Counter Logic Gaming',
-          c: 'Clutch Gaming'
-        },
-        correctAnswer: 'c'
-      },
-      {
-        question: "Who won the last World Cup in 2014?",
-        answers: {
-          a: 'Germany',
-          b: 'Brazil',
-          c: 'Italy'
-        },
-        correctAnswer: 'a'
-      },
-      {
-        question: "What type of bear is best?",
-        answers: {
-          a: 'Brown Bear',
-          b: 'Polar Bear',
-          c: 'False, Black Bear'
-        },
-        correctAnswer: 'c'
-      },
-      {
-        question: "What was the most played Esport in 2017?",
-        answers: {
-          a: 'Dota 2',
-          b: 'League of Legends',
-          c: 'Counter-Strike: Global Offensive'
-        },
-        correctAnswer: 'a'
-      },
-      {
-        question: "What was the highest grossing movie of 2017?",
-        answers: {
-          a: 'Fast and Furious 8',
-          b: 'Star Wars: Episode 8',
-          c: 'Spider-Man Homecoming'
-        },
-        correctAnswer: 'b'
-    }
-    ];
+var questions = [{
+	"question": "Who was the last team to go undefeated in the Premier League?",
+	"option1": "Chelsea",
+	"option2": "Manchester City",
+	"option3": "Arsenal",
+	"option4": "Manchester United",
+	"answer": "2"
+}, {
+	"question": "What was the most played Esports game of 2017?",
+	"option1": "League of Legends",
+	"option2": "Counter-Strike: Global Offensive",
+	"option3": "Overwatch",
+	"option4": "Dota 2",
+	"answer": "3"
+}, {
+	"question": "What type of bear is best?",
+	"option1": "Polar Bear",
+	"option2": "False, Black Bear",
+	"option3": "Panda",
+	"option4": "Grizzley",
+	"answer": "1"
+}, {
+	"question": "What was the highest grossing movie of 2017?",
+	"option1": "Fast and Furious 8",
+	"option2": "Star Wars: Episode 8",
+	"option3": "Wonder Woman",
+	"option4": "Spider-Man Homecoming",
+	"answer": "1"
+}, {
+	"question": "What WWE superstar is not an actor these days?",
+	"option1": "Batista",
+	"option2": "John Cena",
+	"option3": "Dwayne The Rock Johnson",
+	"option4": "Rey Mysterio",
+	"answer": "3"
+}, {
+	"question": "What NALCS team is the first to elminate TSM in a round before the finals?",
+	"option1": "Cloud 9",
+	"option2": "Clutch Gaming",
+	"option3": "Counter Logic Gaming",
+	"option4": "Echo Fox",
+	"answer": "1"
+}, {
+	"question": "Who won the World Cup in 2014?",
+	"option1": "France",
+	"option2": "Brazil",
+	"option3": "Italy",
+	"option4": "Germany",
+	"answer": "3"
+}, {
+	"question": "Who was the number 2 pick in the NBA draft in 2017?",
+	"option1": "Lavar Ball",
+	"option2": "Lonzo Ball",
+	"option3": "Jayson Tatum",
+	"option4": "Donovan Mitchell",
+	"answer": "1"
+}, {
+	"question": "Who is the most recent player to average a triple double in the NBA regular season?",
+	"option1": "Lebron James",
+	"option2": "James Harden",
+	"option3": "Ben Simmons",
+	"option4": "Russell Westbrook",
+	"answer": "3"
+}, {
+	"question": "Who finished the 2017 calendar year with the most goals?",
+	"option1": "Lionel Messi",
+	"option2": "Neymar",
+	"option3": "Cristiano Ronaldo",
+	"option4": "Harry Kane",
+	"answer": "3"
+}];
 
-// this is putting all the informatinon in the divs with the id names quiz results and submit 
-var quizContainer = document.getElementById('quiz');
-var resultsContainer = document.getElementById('results');
-var submitButton = document.getElementById('submit');
+var currentQuestion = 0;
+var score = 0;
+var totQuestions = questions.length;
 
-generateQuiz(myQuestions, quizContainer, resultsContainer, submitButton);
+var container = document.getElementById('quizContainer');
+var questionEl = document.getElementById('question');
+var opt1 = document.getElementById('opt1');
+var opt2 = document.getElementById('opt2');
+var opt3 = document.getElementById('opt3');
+var opt4 = document.getElementById('opt4');
+var nextButton = document.getElementById('nextButton');
+var resultCont = document.getElementById('result');
 
-function generateQuiz(questions, quizContainer, resultsContainer, submitButton){
+function loadQuestion (questionIndex) {
+	var q = questions[questionIndex];
+	questionEl.textContent = (questionIndex + 1) + '. ' + q.question;
+	opt1.textContent = q.option1;
+	opt2.textContent = q.option2;
+	opt3.textContent = q.option3;
+	opt4.textContent = q.option4;
+};
 
-    function showQuestions(questions, quizContainer){
-        var output = [];
-        var answers;
-
-        for(var i=0; i<questions.length; i++){
-            
-            answers = [];
-
-            for(letter in questions[i].answers){
-
-                answers.push(
-                    '<label>'
-                        + '<input type="radio" name="question'+i+'" value="'+letter+'">'
-                        + letter + ': '
-                        + questions[i].answers[letter]
-                    + '</label>'
-                );
-            }
-
-            output.push(
-                '<div class="question">' + questions[i].question + '</div>'
-                + '<div class="answers">' + answers.join('') + '</div>'
-            );
-        }
-
-        quizContainer.innerHTML = output.join('');
-    }
-
-
-    function showResults(questions, quizContainer, resultsContainer){
-        
-        var answerContainers = quizContainer.querySelectorAll('.answers');
-        
-        var userAnswer = '';
-        var numCorrect = 0;
-        
-        for(var i=0; i<questions.length; i++){
-
-            userAnswer = (answerContainers[i].querySelector('input[name=question'+i+']:checked')||{}).value;
-            
-            if(userAnswer===questions[i].correctAnswer){
-                numCorrect++;
-                
-                answerContainers[i].style.color = 'lightgreen';
-            }
-            else{
-                answerContainers[i].style.color = 'red';
-            }
-        }
-
-        resultsContainer.innerHTML = numCorrect + ' out of ' + questions.length;
-    }
-
-    showQuestions(questions, quizContainer);
-    
-    submitButton.onclick = function(){
-        showResults(questions, quizContainer, resultsContainer);
-    }
-
+function loadNextQuestion () {
+	var selectedOption = document.querySelector('input[type=radio]:checked');
+	if(!selectedOption){
+		alert('Select an answer!');
+		return;
+	}
+	var answer = selectedOption.value;
+	if(questions[currentQuestion].answer == answer){
+		score += 10;
+	}
+	selectedOption.checked = false;
+	currentQuestion++;
+	if(currentQuestion == totQuestions - 1){
+		nextButton.textContent = 'Finish';
+	}
+	if(currentQuestion == totQuestions){
+		container.style.display = 'none';
+		resultCont.style.display = '';
+		resultCont.textContent = 'Your Score: ' + score;
+		return;
+	}
+	loadQuestion(currentQuestion);
 }
+
+loadQuestion(currentQuestion);
